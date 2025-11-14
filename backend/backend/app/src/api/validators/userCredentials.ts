4883e01836c55939df513e8b170082aeb70adecc
Khiar Oussama <ossama.khiar@gmail.com>
2025-11-14 23:24:20 +0100

    feat: integrate zxcvbn password strength estimation

--- backend/backend/app/src/api/validators/userCredentials.ts
+++ backend/backend/app/src/api/validators/userCredentials.ts
@@ -1,3 +1,5 @@
+import zxcvbn from 'zxcvbn';
+
 export function isUsernameValid(username: string): boolean {
     const usernameRegexp = /^(?!_)(?!.*__)[a-zA-Z0-9_]{4,12}(?<!_)$/
 
@@ -9,13 +11,17 @@ export function isUsernameValid(username: string): boolean {
 }
 
 export function isPasswordValid(password: string): boolean {
+    // * password policy
     const passwordRegexp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()])[a-zA-Z\d!@#$%^&*() ]{12,28}$/;
 
     if (passwordRegexp.test(password) === false) {
         console.log('invalid password');
+        return (false);
     }
 
-    return (passwordRegexp.test(password));
+    const password_estimation = zxcvbn(password)
+    // TODO :: return feedback as well and display it to the user in the UI, password_estimation.feedback
+    return (password_estimation.score >= 3);
 }
 
 export function isEmailFormatValid(email: string): boolean {

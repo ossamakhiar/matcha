7b412119196daf5923ecb232104190bd93616cd0
Oussama Khiar <okhiar@c3r8p9.1337.ma>
2025-12-27 17:21:43 +0100

    chore: refactor database schema application process

--- backend/database/migrations/00_user_and_messages.sql
+++ backend/database/migrations/00_user_and_messages.sql
@@ -1,15 +1,3 @@
--- Create database owned by the user
-CREATE DATABASE ${POSTGRES_DB} OWNER ${POSTGRES_USER};
-
--- Create user
-CREATE USER ${POSTGRES_USER} WITH PASSWORD '${POSTGRES_PASSWORD}';
-
--- Grant all privileges on all tables in the database to the user
-GRANT ALL PRIVILEGES ON DATABASE ${POSTGRES_DB} TO ${POSTGRES_USER};
-
-\c ${POSTGRES_DB};
-
-
 -- TYPES
 CREATE TYPE MESSAGE_STATUS AS ENUM ('unread', 'read');
 
@@ -118,42 +106,4 @@ CREATE TABLE user_favorite_contacts (
     FOREIGN KEY (user_id) REFERENCES "user" (id) ON DELETE CASCADE,
     FOREIGN KEY (favorite_user_id) REFERENCES "user" (id) ON DELETE CASCADE,
     UNIQUE (user_id, favorite_user_id)
-);
-
-
-CREATE TABLE notification_types (
-    id SERIAL PRIMARY KEY,
-    type VARCHAR(255) NOT NULL UNIQUE,
-    title VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
-);
-
-
-INSERT INTO notification_types (type, title, description) VALUES
-('profile_visit', 'Profile Visited', '{actor_name} visited your profile.'),
-('new_message', 'New Message', 'You have received a new message from {actor_name}.'),
-('like', 'New Like', '{actor_name} liked your profile.'),
-('unlike', 'Like Removed', '{actor_name} removed their like from your profile.');
-
-
-CREATE TABLE notifications (
-    id SERIAL PRIMARY KEY,
-    actor_id INTEGER NOT NULL,
-    notifier_id INTEGER NOT NULL,
-    notification_type_id INTEGER NOT NULL,
-    status BOOLEAN DEFAULT FALSE, -- Read/unread status of the notification
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (actor_id) REFERENCES "user" (id) ON DELETE CASCADE,
-    FOREIGN KEY (notifier_id) REFERENCES "user" (id) ON DELETE CASCADE,
-    FOREIGN KEY (notification_type_id) REFERENCES notification_types (id) ON DELETE SET NULL
-);
-
-CREATE TABLE fake_account_report (
-    id SERIAL PRIMARY KEY,
-    reporting_user_id INTEGER NOT NULL,
-    reported_user_id INTEGER NOT NULL,
-    blocked_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (reporting_user_id) REFERENCES "user" (id) ON DELETE CASCADE,
-    FOREIGN KEY (reported_user_id) REFERENCES "user" (id) ON DELETE CASCADE,
-    UNIQUE (reporting_user_id, reported_user_id)
 );

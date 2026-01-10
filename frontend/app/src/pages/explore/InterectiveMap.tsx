91588b8e0176f6b01d3bf4341558a123521808f5
Oussama Khiar <okhiar@c3r4p9.1337.ma>
2026-01-10 14:32:50 +0100

    remove CITIES constant

--- frontend/app/src/pages/explore/InterectiveMap.tsx
+++ frontend/app/src/pages/explore/InterectiveMap.tsx
@@ -22,36 +22,6 @@ type InteractiveMapProps = {
   onClose: () => void;
 };
 
-const CITIES = [
-  {
-    city: "New York",
-    population: "8,175,133",
-    image:
-      "http://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Above_Gotham.jpg/240px-Above_Gotham.jpg",
-    state: "New York",
-    latitude: 40.6643,
-    longitude: -73.9385,
-  },
-  {
-    city: "Los Angeles",
-    population: "3,792,621",
-    image:
-      "http://upload.wikimedia.org/wikipedia/commons/thumb/5/57/LA_Skyline_Mountains2.jpg/240px-LA_Skyline_Mountains2.jpg",
-    state: "California",
-    latitude: 34.0194,
-    longitude: -118.4108,
-  },
-  {
-    city: "Chicago",
-    population: "2,695,598",
-    image:
-      "http://upload.wikimedia.org/wikipedia/commons/thumb/8/85/2008-06-10_3000x1000_chicago_skyline.jpg/240px-2008-06-10_3000x1000_chicago_skyline.jpg",
-    state: "Illinois",
-    latitude: 41.8376,
-    longitude: -87.6818,
-  },
-];
-
 // TODO : might worth adding an endpoint, to return all of them directly.
 function useSearchAllUsers(): UserInfos[] {
   const [allUsers, setUsers] = useState<UserInfos[]>([]);
